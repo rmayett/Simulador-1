057a93b0c27afbfd528447428fa1bf463d90dbb4
rmayett <rmayett@comunidad.unam.mx>
2017-03-08 21:07:17 -0600

    RR checar metodos de las clases para la cola

---
+++ Robin.java
@@ -0,0 +1,10 @@
+package robin;
+import java.util.LinkedList;// lista
+import java.util.Queue;//cola 
+public class Robin {
+   
+    public static void main(String[] args) {
+        
+    }
+    
+}
