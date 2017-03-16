f045ff8a30fb6270cd99f72dd465addeb44bdc0b
rmayett <rmayett@comunidad.unam.mx>
2017-03-15 18:07:06 -0600

    Round Robin

--- Robin.java
+++ Robin.java
@@ -9,10 +9,26 @@ public class Robin {
         int lim = (int) Math.floor(Math.random()*32);
     	Scanner tec = new Scanner(System.in);
     	do{
+            s.VaciarCola();
     		System.out.println("\tSimulador de Planificador Round Robin\nSelecciona una Opcion para continuar:\n1.-ingresar nombres de procesos\n2.-Correr simulador con numeros aleatorios\n3.-Salir");  
     		op=tec.nextInt();
     		switch(op){
     			case 1:
+                int proce,id,rafaga,prioridad,tamanio;
+                System.out.println("Dame el numero de procesos que quieres crear:");
+                proce = tec.nextInt();
+                for (int j=0;j<proce;j++) {
+                    System.out.println("Dame los sig datos:\n id:");
+                    id= tec.nextInt();
+                    System.out.println("Rafaga:");
+                    rafaga= tec.nextInt();
+                    System.out.println("Prioridad:");
+                    prioridad= tec.nextInt();
+                    System.out.println("Tamaño:");
+                    tamanio= tec.nextInt();
+                    s.push(id,rafaga,rafaga,prioridad,tamanio);                    
+                }
+                r.robinu(s,proce);
     			break;
     			case 2:
                 for (int i =0;i<lim;i++) {
@@ -29,24 +45,50 @@ public class Robin {
     	}while(op!=3);
     }
 
-    public void robin(Cola r,int lim){    
-    	//probando la clase cola
-        int aux,mb=0; 
-        for (int i=0;i<lim;i++) {
-            for (int j=0;j<r.tope().getrafaga()-1;j++) {
+    public void robin(Cola r,int lim){ 
+        int aux; 
+        Proceso auxp = new Proceso();
+       // for (int i=0;i<lim;i++) {
+          //  for (int j=0;j<r.tope().getrafaga()-1;j++) {
+        do{
             aux=r.tope().gettiempo();
             aux=aux-4;
+            auxp=r.pop();
+            r.push(auxp.getid(),auxp.getrafaga(),auxp.gettiempo(),auxp.getprioridad(),auxp.gettamanio());
             r.tope().settiempo(aux);
             if (aux>0) {
                 System.out.println("Nombre del Proceso: "+ r.tope().getnombre() +" id: "+ r.tope().getid() +" rafaga: "+ r.tope().getrafaga() +" tiempo restante: "+r.tope().gettiempo()+" Prioridad: "+r.tope().getprioridad()+" Tamaño: "+r.tope().gettamanio()+"MB");            
             }
-            if (aux<0) {                
+            if (aux<=0) {                
                 System.out.println("Nombre del Proceso: "+ r.tope().getnombre() +" id: "+ r.tope().getid() +" rafaga: "+ r.tope().getrafaga() +" tiempo restante: 0 Prioridad: "+r.tope().getprioridad()+" Tamaño: "+r.tope().gettamanio()+"MB");                         
             }
             if (r.tope().gettiempo()<=0) {
                 r.pop();                
             }                
+            //}
+        }while(r.tope().getsig()!=null);    
+    }
+    public void robinu(Cola r,int lim){ 
+        int aux; 
+        Proceso auxp = new Proceso();
+       // for (int i=0;i<lim;i++) {
+          //  for (int j=0;j<r.tope().getrafaga()-1;j++) {
+        do{
+            aux=r.tope().gettiempo();
+            aux=aux-4;
+            auxp=r.pop();
+            r.push(auxp.getid(),auxp.getrafaga(),auxp.gettiempo(),auxp.getprioridad(),auxp.gettamanio());
+            r.tope().settiempo(aux);
+            if (aux>0) {
+                System.out.println("Nombre del Proceso: "+ r.tope().getnombre() +" id: "+ r.tope().getid() +" rafaga: "+ r.tope().getrafaga() +" tiempo restante: "+r.tope().gettiempo()+" Prioridad: "+r.tope().getprioridad()+" Tamaño: "+r.tope().gettamanio()+"MB");            
             }
-        }    
+            if (aux<=0) {                
+                System.out.println("Nombre del Proceso: "+ r.tope().getnombre() +" id: "+ r.tope().getid() +" rafaga: "+ r.tope().getrafaga() +" tiempo restante: 0 Prioridad: "+r.tope().getprioridad()+" Tamaño: "+r.tope().gettamanio()+"MB");                         
+            }
+            if (r.tope().gettiempo()<=0) {
+                r.pop();                
+            }                
+            //}
+        }while(r.tope().getsig()!=null);    
     }    
 }
